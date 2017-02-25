461677628035e24fbed836197c4e06343ab0d364
Nathan Hugon <nthugon@gmail.com>
2017-02-25 14:38:54 -0800

    Card class passes unit test

---
+++ js/card.js
@@ -0,0 +1,15 @@
+(function(exports) {
+
+    class Card {
+        constructor(suit, rank) {
+            this.suit = suit;
+            this.rank = rank;
+        }
+    }
+
+    exports.Card = Card;
+
+
+})(this);
+
+
---
+++ test/card.test.js
@@ -0,0 +1,18 @@
+const assert = chai.assert;
+
+describe('Card class', () => {
+
+    it('makes card with given suit and rank', done => {
+        let suit = "hearts";
+        let rank = 13;
+        let card = new Card(suit, rank);
+        assert.equal(suit, card.suit);
+        assert.equal(rank, card.rank);
+        done();
+    });
+
+});
+
+
+
+
